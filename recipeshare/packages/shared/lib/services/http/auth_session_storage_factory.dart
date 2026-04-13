f6432becace8299858708802db9cf03349469338
inescodefam <ines.codefam@gmail.com>
2026-04-13 21:20:18 +0200

    refactor: Update auth session storage

--- recipeshare/packages/shared/lib/services/http/auth_session_storage_factory.dart
+++ recipeshare/packages/shared/lib/services/http/auth_session_storage_factory.dart
@@ -1,5 +1,6 @@
 import 'auth_session_storage.dart';
 import 'auth_session_storage_io.dart'
-    if (dart.library.html) 'auth_session_storage_web.dart' as _impl;
+    if (dart.library.html) 'auth_session_storage_web.dart' as impl;
 
-AuthSessionStorage createAuthSessionStorage() => _impl.createAuthSessionStorage();
+/// Platform-specific storage: cookies on web, secure storage on IO.
+AuthSessionStorage createAuthSessionStorage() => impl.createAuthSessionStorage();
